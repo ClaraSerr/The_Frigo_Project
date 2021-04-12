a71858f0b1a201486a2693a350666417a90e3260
ClaraSerr <clara.serruau4@etu.univ-lorraine.fr>
2021-04-12 17:01:47 +0200

    N'accepte plus les ingrédients non présents

--- src/Components/ComboBox/ComboBox.js
+++ src/Components/ComboBox/ComboBox.js
@@ -10,6 +10,14 @@ const [inputValue, setInputValue] = React.useState('');
 
 const ingr = []
 
+function affichage(arg){
+  if(typeof arg!="undefined"){
+    return(
+      <ListMeals data={arg.strIngredient}></ListMeals>
+    )
+  }  
+}
+
 for (const [index, valeur] of props.data.entries()) {
   ingr.push(valeur.strIngredient)
 }
@@ -20,19 +28,20 @@ for (const [index, valeur] of props.data.entries()) {
       id="combo-box-ingr"
       value={value}
         onChange={(event, newValue) => {
-            setValue(newValue);
+          if(newValue!=null){
+            setValue(newValue);}
         }}
         onInputChange={(event, newInputValue) => {
-          if(newInputValue!=""){
+          if(newInputValue!=null){
             setInputValue(newInputValue);
         }}}
       options={props.data}
       getOptionLabel={(option) => option.strIngredient}
       style={{ width: 300 }}
       renderInput={(params) => <TextField {...params} label="Ingredient" variant="outlined" 
-      onKeyDown={(e) => {if (e.key === 'Enter') setValue(props.data.find((element)=>element.strIngredient==inputValue))}}/>}
+      onKeyDown={(e) => {if (e.key === 'Enter' && (ingr.includes(inputValue))) setValue(props.data.find((element)=>element.strIngredient==inputValue))}}/>}
     />
-    <ListMeals data={value.strIngredient}></ListMeals>
+    <div>{affichage(value)}</div>
     </div>
 
   );
