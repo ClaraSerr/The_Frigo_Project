4d0b2709d8d8e04884cf28b2a07d3bead2d3bede
ClaraSerr <clara.serruau4@etu.univ-lorraine.fr>
2021-05-17 13:02:16 +0200

    add link

--- src/Components/Meals/Meals.js
+++ src/Components/Meals/Meals.js
@@ -4,8 +4,10 @@ import "./meals.css";
 const Meals = (props) => {
   return (
     <div className="results">
+      <a className="test" href={"https://www.themealdb.com/meal.php?c="+props.data[2]} target="_blank">
       <img src={props.data[1]} className="sprite" />
       <div className="legende">{props.data[0]}</div>
+      </a>
     </div>
   );
 };
